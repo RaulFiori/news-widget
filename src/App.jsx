e6cceaebc3b0850c367209ac4878d5e88d023b1c
Raul Fiori <raulproc1807@gmail.com>
2019-08-17 16:47:26 -0300

    initial setup of project linter and prettier added

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <h1> Hello, World! </h1>
+//       </div>
+//     );
+//   }
+// }
+
+const App = () => (
+  <div>
+    <h1> Hello, World! </h1>
+  </div>
+);
+
+export default hot(module)(App);
